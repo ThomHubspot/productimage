b9aa09e98834275d8770670b7e401ea0962c4802
ThomHubspot <129523852+ThomHubspot@users.noreply.github.com>
2025-03-12 12:17:17 +0100

    Update PropertyTile.tsx

--- src/app/extensions/components/PropertyTile.tsx
+++ src/app/extensions/components/PropertyTile.tsx
@@ -38,51 +38,7 @@ export const PropertyTile = ({ listingItem }: PropertyTileProps) => {
           width={300}
           height={180}
         />
-        <Box>
-          {/* Set the arrangement of elements like column, wrap in box components to combine elements into sets to set the spacing between. */}
-          <Flex direction="column" gap="xs">
-            <Box>
-              <Flex justify="between">
-               
-                <Tag variant={listingItem.status}>
-                  {findLabelByValue(
-                    listingItemStatusSelectOptions,
-                    listingItem.status,
-                  )}
-                </Tag>
-              </Flex>
-            </Box>
-            <Text>
-              {findLabelByValue(
-                propertyTypeSelectOptions,
-                listingItem.propertyType,
-              )}
-            </Text>
-            <Text>
-              {truncateWithEllipsis(listingItem.address, maxAddressLength)}
-            </Text>
-            <Box>
-              {/* Set the arrangement of elements like column. */}
-              <Flex direction="column">
-                <Text variant="microcopy">
-                  {`Сreated ${listingItem.creationDate.formattedDate}`}
-                </Text>
-                <Text variant="microcopy">
-                  <Link href="">Contact listing owner</Link>
-                </Text>
-              </Flex>
-            </Box>
-            <Button
-              disabled={
-                listingItem.status !== ListingItemStatus.Undefined &&
-                listingItem.status !== ListingItemStatus.Available
-              }
-              variant="secondary"
-            >
-              View property
-            </Button>
-          </Flex>
-        </Box>
+        
       </Flex>
     </Tile>
   );
